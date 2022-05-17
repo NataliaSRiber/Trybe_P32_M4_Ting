54025a3a0ae1deb7ea4c304de0f9713e42cb791c
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-05-17 14:31:30 -0300

    feat: adds requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for content in instance.data:
+        if content["nome_do_arquivo"] == path_file:
+            return
+    listed_file = txt_importer(path_file)
+
+    new_information = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(listed_file),
+        "linhas_do_arquivo": listed_file,
+    }
+    instance.enqueue(new_information)
+    sys.stdout.write(f'{new_information}')
 
 
 def remove(instance):
